81d15218baeb2c0d2b60c96b7faca84614543b4e
zamniz88-dot <zam.niz88@gmail.com>
2025-12-28 11:28:22 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello,world")
+
+# Printing my name 
+print("My name is Sahil.")
+
+# Printing a fun fact about myself
+print("I love playing football.")
+
+# Performing and printing a simple calculation
+print("Thhe result of 3 + 5is:",3 + 5)
---
+++ pythonkeyword.py
@@ -0,0 +1,13 @@
+#Print a Sentence
+print("Welcome to the world of programing!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Printing all the keywords present in python
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
