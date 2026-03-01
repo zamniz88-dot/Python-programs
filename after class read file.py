8d615a4164067a3a57c3eb789fa6dd86d9dda62d
zamniz88-dot <zam.niz88@gmail.com>
2026-03-01 11:13:22 +0000

    Add files via upload

---
+++ after class read file.py
@@ -0,0 +1,12 @@
+file = open('Hand written.txt','r')
+print(file.read())
+file.close()
+
+file = open('Hand written.txt','r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+file = open('Codingal.txt','a')
+file.write("Hi! I am Penguin and I am 1 yr old.")
+file.close()
